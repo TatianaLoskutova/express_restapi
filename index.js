afa3152b5e6fd99112a464328dfa8d88ef84be0a
TatianaLoskutova <tanya.loskutova333@gmail.com>
2023-05-07 16:24:10 +0300

    Express and nodemon added

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    const a = 4;
+    if (a > 5) {
+        res.send('OK!')
+    } else {
+        res.send('Hello World!')
+    }
+})
+
+app.get('/samurais', (req, res) => {
+    res.send('Hello samurai, Tanya!')
+})
+
+app.post('/samurais', (req, res) => {
+    res.send('We have created samurai!!!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
